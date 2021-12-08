846b76a358c28c5aac2f234f3b50374fe0c28dd7
weicheng lee <z82111712@gmail.com>
2021-12-08 14:21:15 -0800

    language controller added

---
+++ src/main/java/com/wei/languages/repositories/LanguageRepository.java
@@ -0,0 +1,13 @@
+package com.wei.languages.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wei.languages.models.Language;
+
+@Repository
+public interface LanguageRepository extends CrudRepository<Language, Long> {
+	List<Language> findAll();
+}
---
+++ src/main/java/com/wei/languages/services/LanguageService.java
@@ -0,0 +1,8 @@
+package com.wei.languages.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LanguageService {
+
+}
